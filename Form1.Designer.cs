32db0e1604820e16ae42de4e2ab132c6e2c09348
normannder <deman.channel@gmail.com>
2020-04-03 21:32:09 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Dice_Poker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxPL2 = new System.Windows.Forms.GroupBox();
+            this.pl2Pic1 = new System.Windows.Forms.PictureBox();
+            this.pl2Pic2 = new System.Windows.Forms.PictureBox();
+            this.pl2Pic3 = new System.Windows.Forms.PictureBox();
+            this.pl2Pic4 = new System.Windows.Forms.PictureBox();
+            this.pl2Pic5 = new System.Windows.Forms.PictureBox();
+            this.groupBoxPL1 = new System.Windows.Forms.GroupBox();
+            this.pl1Pic1 = new System.Windows.Forms.PictureBox();
+            this.pl1Pic2 = new System.Windows.Forms.PictureBox();
+            this.pl1Pic3 = new System.Windows.Forms.PictureBox();
+            this.pl1Pic4 = new System.Windows.Forms.PictureBox();
+            this.pl1Pic5 = new System.Windows.Forms.PictureBox();
+            this.mainHintLabel = new System.Windows.Forms.Label();
+            this.throwButton = new System.Windows.Forms.Button();
+            this.combinationBox = new System.Windows.Forms.ComboBox();
+            this.player1Hint = new System.Windows.Forms.Label();
+            this.player2Hint = new System.Windows.Forms.Label();
+            this.groupBoxPL2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic5)).BeginInit();
+            this.groupBoxPL1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxPL2
+            // 
+            this.groupBoxPL2.Controls.Add(this.pl2Pic1);
+            this.groupBoxPL2.Controls.Add(this.pl2Pic2);
+            this.groupBoxPL2.Controls.Add(this.pl2Pic3);
+            this.groupBoxPL2.Controls.Add(this.pl2Pic4);
+            this.groupBoxPL2.Controls.Add(this.pl2Pic5);
+            this.groupBoxPL2.Location = new System.Drawing.Point(132, 81);
+            this.groupBoxPL2.Name = "groupBoxPL2";
+            this.groupBoxPL2.Size = new System.Drawing.Size(630, 100);
+            this.groupBoxPL2.TabIndex = 0;
+            this.groupBoxPL2.TabStop = false;
+            this.groupBoxPL2.Text = "Игрок 2";
+            // 
+            // pl2Pic1
+            // 
+            this.pl2Pic1.Location = new System.Drawing.Point(22, 19);
+            this.pl2Pic1.Name = "pl2Pic1";
+            this.pl2Pic1.Size = new System.Drawing.Size(70, 70);
+            this.pl2Pic1.TabIndex = 0;
+            this.pl2Pic1.TabStop = false;
+            // 
+            // pl2Pic2
+            // 
+            this.pl2Pic2.Location = new System.Drawing.Point(150, 19);
+            this.pl2Pic2.Name = "pl2Pic2";
+            this.pl2Pic2.Size = new System.Drawing.Size(70, 70);
+            this.pl2Pic2.TabIndex = 1;
+            this.pl2Pic2.TabStop = false;
+            // 
+            // pl2Pic3
+            // 
+            this.pl2Pic3.Location = new System.Drawing.Point(282, 19);
+            this.pl2Pic3.Name = "pl2Pic3";
+            this.pl2Pic3.Size = new System.Drawing.Size(70, 70);
+            this.pl2Pic3.TabIndex = 2;
+            this.pl2Pic3.TabStop = false;
+            // 
+            // pl2Pic4
+            // 
+            this.pl2Pic4.Location = new System.Drawing.Point(404, 19);
+            this.pl2Pic4.Name = "pl2Pic4";
+            this.pl2Pic4.Size = new System.Drawing.Size(70, 70);
+            this.pl2Pic4.TabIndex = 3;
+            this.pl2Pic4.TabStop = false;
+            // 
+            // pl2Pic5
+            // 
+            this.pl2Pic5.Location = new System.Drawing.Point(530, 19);
+            this.pl2Pic5.Name = "pl2Pic5";
+            this.pl2Pic5.Size = new System.Drawing.Size(70, 70);
+            this.pl2Pic5.TabIndex = 4;
+            this.pl2Pic5.TabStop = false;
+            // 
+            // groupBoxPL1
+            // 
+            this.groupBoxPL1.Controls.Add(this.pl1Pic1);
+            this.groupBoxPL1.Controls.Add(this.pl1Pic2);
+            this.groupBoxPL1.Controls.Add(this.pl1Pic3);
+            this.groupBoxPL1.Controls.Add(this.pl1Pic4);
+            this.groupBoxPL1.Controls.Add(this.pl1Pic5);
+            this.groupBoxPL1.Location = new System.Drawing.Point(132, 299);
+            this.groupBoxPL1.Name = "groupBoxPL1";
+            this.groupBoxPL1.Size = new System.Drawing.Size(630, 100);
+            this.groupBoxPL1.TabIndex = 1;
+            this.groupBoxPL1.TabStop = false;
+            this.groupBoxPL1.Text = "Игрок 1";
+            // 
+            // pl1Pic1
+            // 
+            this.pl1Pic1.Location = new System.Drawing.Point(22, 19);
+            this.pl1Pic1.Name = "pl1Pic1";
+            this.pl1Pic1.Size = new System.Drawing.Size(70, 70);
+            this.pl1Pic1.TabIndex = 5;
+            this.pl1Pic1.TabStop = false;
+            // 
+            // pl1Pic2
+            // 
+            this.pl1Pic2.Location = new System.Drawing.Point(150, 19);
+            this.pl1Pic2.Name = "pl1Pic2";
+            this.pl1Pic2.Size = new System.Drawing.Size(70, 70);
+            this.pl1Pic2.TabIndex = 6;
+            this.pl1Pic2.TabStop = false;
+            // 
+            // pl1Pic3
+            // 
+            this.pl1Pic3.Location = new System.Drawing.Point(282, 19);
+            this.pl1Pic3.Name = "pl1Pic3";
+            this.pl1Pic3.Size = new System.Drawing.Size(70, 70);
+            this.pl1Pic3.TabIndex = 7;
+            this.pl1Pic3.TabStop = false;
+            // 
+            // pl1Pic4
+            // 
+            this.pl1Pic4.Location = new System.Drawing.Point(404, 19);
+            this.pl1Pic4.Name = "pl1Pic4";
+            this.pl1Pic4.Size = new System.Drawing.Size(70, 70);
+            this.pl1Pic4.TabIndex = 8;
+            this.pl1Pic4.TabStop = false;
+            // 
+            // pl1Pic5
+            // 
+            this.pl1Pic5.Location = new System.Drawing.Point(530, 19);
+            this.pl1Pic5.Name = "pl1Pic5";
+            this.pl1Pic5.Size = new System.Drawing.Size(70, 70);
+            this.pl1Pic5.TabIndex = 9;
+            this.pl1Pic5.TabStop = false;
+            // 
+            // mainHintLabel
+            // 
+            this.mainHintLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainHintLabel.Location = new System.Drawing.Point(132, 19);
+            this.mainHintLabel.Name = "mainHintLabel";
+            this.mainHintLabel.Size = new System.Drawing.Size(630, 37);
+            this.mainHintLabel.TabIndex = 2;
+            this.mainHintLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // throwButton
+            // 
+            this.throwButton.Location = new System.Drawing.Point(308, 221);
+            this.throwButton.Name = "throwButton";
+            this.throwButton.Size = new System.Drawing.Size(250, 46);
+            this.throwButton.TabIndex = 3;
+            this.throwButton.Text = "Бросить кубики";
+            this.throwButton.UseVisualStyleBackColor = true;
+            this.throwButton.Click += new System.EventHandler(this.ThrowButton_Click);
+            // 
+            // combinationBox
+            // 
+            this.combinationBox.FormattingEnabled = true;
+            this.combinationBox.Items.AddRange(new object[] {
+            "Покер",
+            "Карэ",
+            "Фулл Хаус",
+            "Большой стрит",
+            "Малый стрит",
+            "Сет",
+            "Пара"});
+            this.combinationBox.Location = new System.Drawing.Point(357, 54);
+            this.combinationBox.Name = "combinationBox";
+            this.combinationBox.Size = new System.Drawing.Size(181, 21);
+            this.combinationBox.TabIndex = 4;
+            this.combinationBox.Text = "Выберите комбинацию";
+            // 
+            // player1Hint
+            // 
+            this.player1Hint.AutoSize = true;
+            this.player1Hint.Location = new System.Drawing.Point(132, 424);
+            this.player1Hint.Name = "player1Hint";
+            this.player1Hint.Size = new System.Drawing.Size(110, 13);
+            this.player1Hint.TabIndex = 5;
+            this.player1Hint.Text = "Подсказка игрока 1";
+            // 
+            // player2Hint
+            // 
+            this.player2Hint.AutoSize = true;
+            this.player2Hint.Location = new System.Drawing.Point(135, 202);
+            this.player2Hint.Name = "player2Hint";
+            this.player2Hint.Size = new System.Drawing.Size(110, 13);
+            this.player2Hint.TabIndex = 6;
+            this.player2Hint.Text = "Подсказка игрока 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 528);
+            this.Controls.Add(this.player2Hint);
+            this.Controls.Add(this.player1Hint);
+            this.Controls.Add(this.combinationBox);
+            this.Controls.Add(this.throwButton);
+            this.Controls.Add(this.mainHintLabel);
+            this.Controls.Add(this.groupBoxPL1);
+            this.Controls.Add(this.groupBoxPL2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxPL2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2Pic5)).EndInit();
+            this.groupBoxPL1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1Pic5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainHintLabel;
+        private System.Windows.Forms.GroupBox groupBoxPL2;
+        private System.Windows.Forms.PictureBox pl2Pic1;
+        private System.Windows.Forms.PictureBox pl2Pic2;
+        private System.Windows.Forms.PictureBox pl2Pic3;
+        private System.Windows.Forms.PictureBox pl2Pic4;
+        private System.Windows.Forms.PictureBox pl2Pic5;
+        private System.Windows.Forms.GroupBox groupBoxPL1;
+        private System.Windows.Forms.PictureBox pl1Pic1;
+        private System.Windows.Forms.PictureBox pl1Pic2;
+        private System.Windows.Forms.PictureBox pl1Pic3;
+        private System.Windows.Forms.PictureBox pl1Pic4;
+        private System.Windows.Forms.PictureBox pl1Pic5;
+        private System.Windows.Forms.Button throwButton;
+        private System.Windows.Forms.ComboBox combinationBox;
+        private System.Windows.Forms.Label player1Hint;
+        private System.Windows.Forms.Label player2Hint;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dice_Poker
+{
+    public partial class Form1 : Form
+    {
+        int[] pl1Val = {0,0,0,0,0};
+        int whoseTurn = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            player1Hint.Visible = false;
+            player2Hint.Visible = false;
+        }
+
+        private void ThrowButton_Click(object sender, EventArgs e)
+        {
+            if (whoseTurn % 2 != 0)
+            {
+                GenerateDicePics(groupBoxPL1);
+                mainHintLabel.Text = "Игрок 1, выберите комбинацию, которая у вас выпала!";
+                whoseTurn++;
+                switch(combinationBox.SelectedItem)
+                {
+                    case "Покер":
+                        break;
+                    case "Карэ":
+                        break;
+                    case "Фулл Хаус":
+                        break;
+                    case "Большой стрит":
+                        break;
+                    case "Малый стрит":
+                        break;
+                    case "Сет":
+                        break;
+                    case "Пара":
+                        break;
+                }
+            }
+            else
+            {
+                GenerateDicePics(groupBoxPL2);
+                mainHintLabel.Text = "Игрок 2, выберите комбинацию, которая у вас выпала!";
+                whoseTurn++;
+            }
+        }
+
+
+        async public void GenerateDicePics(GroupBox playersGroupBox)
+        {
+            Random rnd = new Random();
+            int n;
+            foreach (PictureBox pic in playersGroupBox.Controls.OfType<PictureBox>())
+            {
+               n = rnd.Next(1, 7);
+               await (Task.Delay(10));
+               pic.Image = Image.FromFile($@"content\pics\{n}.jpg");
+            }
+        }
+
+        public string GenerateDiceCombination()
+        {
+
+            //Все варианты комбинации "ПАРА"
+            for (int i = 0; i < pl1Val.Length; i++)
+            {
+                for (int j = 0; j < pl1Val.Length; j++)
+                {
+                    if (pl1Val[i] == pl1Val[j])
+                    {
+                        mainHintLabel.Text = "У вас выпала пара!";
+                    }
+                    
+                }
+            } 
+
+            //for (int i = 0; i < pl1Val.Length; i++)
+            //{
+            //    for (int j = 0; j < pl1Val.Length; j++)
+            //    {
+            //        for (int k = 0; k < pl1Val.Length; k++)
+            //        {
+            //            if (pl1Val[i] == pl1Val[j] && pl1Val[i] == pl1Val[k] && pl1Val[k] == pl1Val[j])
+            //            {
+            //                hintLabel.Text = "У вас выпал сет!";
+            //            }
+            //            else hintLabel.Text = "Ничего не выпало";
+            //        }    
+            //    }
+            //}
+
+
+
+            //if (pl1Val[0] == pl1Val[1] || pl1Val[0] == pl1Val[2] || pl1Val[0] == pl1Val[3] || pl1Val[0] == pl1Val[4])
+            //{
+            //    hintLabel.Text = "У вас выпала пара!";
+            //}
+            //else if (pl1Val[1] == pl1Val[1] || pl1Val[1] == pl1Val[2] || pl1Val[1] == pl1Val[3] || pl1Val[1] == pl1Val[4])
+            //{
+            //    hintLabel.Text = "У вас выпала пара!";
+            //}
+            //else if (pl1Val[2] == pl1Val[1] || pl1Val[2] == pl1Val[2] || pl1Val[2] == pl1Val[3] || pl1Val[2] == pl1Val[4])
+            //{
+            //    hintLabel.Text = "У вас выпала пара!";
+            //}
+            //else if (pl1Val[3] == pl1Val[1] || pl1Val[3] == pl1Val[2] || pl1Val[3] == pl1Val[3] || pl1Val[3] == pl1Val[4])
+            //{
+            //    hintLabel.Text = "У вас выпала пара!";
+            //}
+            //else if (pl1Val[4] == pl1Val[1] || pl1Val[4] == pl1Val[2] || pl1Val[4] == pl1Val[3] || pl1Val[4] == pl1Val[4])
+            //{
+            //    hintLabel.Text = "У вас выпала пара!";
+            //}
+            //else if (pl1Val[0] == pl1Val[1] & pl1Val[0] == pl1Val[2])
+            //{
+            //    hintLabel.Text = "У вас выпал сет!";
+            //}
+            //else if (pl1Val[0] == pl1Val[2] & pl1Val[0] == pl1Val[3])
+            //{
+            //    hintLabel.Text = "У вас выпал сет!";
+            //}
+            //else if (pl1Val[0] == pl1Val[3] & pl1Val[0] == pl1Val[4])
+            //{
+            //    hintLabel.Text = "У вас выпал сет!";
+            //}
+
+
+            return mainHintLabel.Text;
+        }
+    }
+
+}
